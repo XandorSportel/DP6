44ae52e5c1edc572981b8d5a07c412e3019421d1
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-02-21 19:54:43 +0100

    feat: updated classes

--- src/classes/Stap.py
+++ src/classes/Stap.py
@@ -6,6 +6,12 @@ class Stap:
         self.__beschrijving = beschrijving
         self.__tip = tip
 
+    def get_beschrijving(self):
+        return self.__beschrijving
+    
+    def get_tip(self):
+        return self.__tip
+
     def __str__(self):
         if self.__tip:
             return f"{self.__beschrijving} ({color('Tip:', Colors.INFO)} {self.__tip})"
